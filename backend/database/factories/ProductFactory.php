a53e7cfd413f4526bb4793258ee8e1b36e39ab2a
Nazam <nazamfr1998@gmail.com>
2022-01-17 15:04:41 +0100

    Added condition if no order

--- backend/database/factories/ProductFactory.php
+++ backend/database/factories/ProductFactory.php
@@ -16,11 +16,13 @@ class ProductFactory extends Factory
     {
         $shop = Shop::inRandomOrder()->withCount('products')->get();
         $shop = $shop->where('products_count', '<', 10)->first();
+        $shopName = ['Barbecue Style', 'Dippers Guacamole', 'Dippers Naturel', 'Flamin Hot', 'Nacho Cheese', 'Pure Paprika', 'Sweet Chilli Pepper'];
         $shopImg = ['barbecue_style.jpg', 'dippers_guacamole.jpg', 'dippers_naturel.jpg', 'flamin_hot.jpg', 'nacho_cheese.jpg', 'pure_paprika.jpg', 'sweet_chilli_pepper.jpg'];
+        $index = array_rand($shopName);
         return [
-            'name' => $this->faker->name(),
+            'name' => $shopName[$index],
             'description' => $this->faker->realText(),
-            'cover_path' => $shopImg[array_rand($shopImg)],
+            'cover_path' => $shopImg[$index],
             'price' => $this->faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 5000),
             'stock' => $this->faker->randomDigitNotNull(),
             'shop_id' => $shop->id
